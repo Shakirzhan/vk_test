d9ea1461fd0dcccd3b64306207a1faa3153ae09d
Shakirzhan <samarbaev99@mail.ru>
2020-06-14 18:54:08 +0300

    Updates

---
+++ precache-manifest.ddeae12ea296f7628b5c76e9ebbd8eb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5271b886c847e150ba9464d8f4700e91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bccce59506439402adb",
+    "url": "/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "5bccce59506439402adb",
+    "url": "/static/js/2.8a22c271.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.8a22c271.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8cb0e07e6d322646dbd",
+    "url": "/static/js/main.fe709b77.chunk.js"
+  },
+  {
+    "revision": "4ff078901909315f41a4",
+    "url": "/static/js/runtime-main.6f538cea.js"
+  }
+]);
